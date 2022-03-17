2418559224077363dd7cd4530fe2f9ecbcd6029c
wangkai53 <wangkai53@sfmail.sf-express.com>
2022-03-17 23:10:33 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+source ~/.bashrc
 pwd
+
+tar -czvf deploy.tar.gz dist/*
+
+scp -r  deploy.tar.gz root@139.196.89.228:~/usr/share/nginx
+tar -xzvf deploy.tar.gz
+cp  -r dist/ ./yanshi
+rm -rf deploy.tar.gz
