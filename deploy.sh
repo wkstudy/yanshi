269d6186b41b2d3849ff5841bfa86fb2e760a62d
wkstudy <1346948470@qq.com>
2022-03-18 07:46:15 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-tar -czvf deploy.tar.gz dist/*
 
-scp -r  deploy.tar.gz root@139.196.89.228:~/usr/share/nginx
-tar -xzvf deploy.tar.gz
-cp  -r dist/ ./yanshi
-rm -rf deploy.tar.gz
+scp -r dist/ root@139.196.89.228:/usr/share/nginx
+
+
