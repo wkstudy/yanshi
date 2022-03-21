523809a74bab30073d1f04fb52904ab0135253eb
wangkai53 <wangkai53@sfmail.sf-express.com>
2022-03-21 21:25:59 +0800

    done

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 pwd
 echo build.sh的env
+echo '环境变量-------------'
 env
+echo '自定义变量------------'
+echo "第一个参数为：$1";
+echo "第二个参数为：$2";
 node -v
 npm install --registry=https://registry.npmmirror.com
 npm run build
